88700c830fe1291517cb782ebc0b5142245a1c8b
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-05-18 19:26:03 +0200

    Update return type

--- app/interfaces/task_queue.py
+++ app/interfaces/task_queue.py
@@ -5,13 +5,15 @@ class TaskQueue(ABC):
     """Abstract base class representing an asynchronous task queue interface."""
 
     @abstractmethod
-    async def enqueue(self, task: dict) -> None:
+    async def enqueue(self, task: dict, queue_name: str = "default") -> int:
         """Asynchronously add a task to the queue.
 
         Parameters
         ----------
         task: dict
             A dictionary representing the task payload.
+        queue_name: str
+            A queue default name.
 
         """
         pass
